00564686e0f541d55113096f4be6218ba811f88b
Steven Romriell <romriellsteven@gmail.com>
2019-05-22 22:31:15 -0400

    newest script

--- practice scripts/nvs_steven522.R
+++ practice scripts/nvs_steven522.R
@@ -6,14 +6,6 @@ library(dplyr)
 library(DT)
 library(tools)
 master1 <- read.csv("c:/Users/romri/Documents/Working/Master.txt")
-# Create alphabetical, unique, sorted list of state abbreviations
-tablestate <- names(table(master1$State))
-# Create alphabetical, unique, sorted list of school names
-tableschoolname <- names(table(master1$School_Name))
-
-# Create alphabetical, unique, sorted list of degree types
-tabledegreename <- names(table(master1$Degree_Name))
-
 
 master2 <- select(master1, State, School_Name, Degree_Name, CIP_Name)
 
@@ -29,31 +21,23 @@ ui <- fluidPage(
       
       selectInput(inputId = "x",
                   label= "State:",
-                  choices = names(table(master2$State)),
+                  choices = names(table(master1$State)),
                   selected = "ID"),
       
       hr(),
       h3("School Name"),
       selectInput(inputId = "y",
                   label = "School Name:",
-                  choices = names(table(master2$School_Name)),
-                  selected = "Idaho State University"),
+                  choices = NULL),
+                 
       
       hr(),
       h3("Degree Type"),
       
       selectInput(inputId = "z",
                   label = "Degree Name:",
-                  choices = names(table(master2$Degree_Name)),
-                  selected = "Associates Degree"),
-      hr(),
-      h3("Course Name"),
-      
-      selectInput(inputId = "w",
-                  label = "Course Name:",
-                  choices = names(table(master2$CIP_Name)),
-                  selected = "Business")
-      
+                  choices = NULL)
+                  
 
   ),
       #output
@@ -67,11 +51,16 @@ ui <- fluidPage(
   
 )
 # Define server function required to create the scatterplot
-server <- function(input, output) {
-  #degree_selected <- reactive ({filter(master2, State %in% input$x & School_Name %in% input$y)
-  #})
-
-  # Print data table if checked
+server <- function(input, output, session) {
+  observe({
+    y_school <- master1 %>% filter(State == input$x) %>% select(School_Name)
+    updateSelectInput(session, "y", "School Name:", choices = unique(y_school))
+  })
+  observe({
+    z_degree <- master1$Degree_Name[master1$School_Name == input$y]
+    updateSelectInput(session, "z", "Degree Name:", choices = unique(z_degree))
+  })
+  # Print data table if 
   output$degreetable <- renderDataTable({
       DT::datatable(data = filter(master1, State == input$x, School_Name == input$y, Degree_Name == input$z),
                     options = list(pageLength = 20)                )
